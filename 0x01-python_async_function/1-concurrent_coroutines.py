ebc68debbcab3bddc851ffe0a2e6bfeb77618cba
jack-njenga <jacknjenga99@gmail.com>
2023-12-11 15:24:39 +0300

    variable declaration

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,11 @@ write an async routine called wait_n that takes in 2 int arguments
 You will spawn wait_random n times with the specified max_delay.
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     return the list of all the delays (float values)
     """
